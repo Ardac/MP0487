bebfbbb4ef11b3cde5cfd78053d2524594659abd
miguel <mtuimil@gmail.com>
2016-03-06 13:05:32 +0100

    Mod - Creciacion de metodo en clase main, Paso 2 tarea

--- src/dam_ed04_actividad/Main.java
+++ src/dam_ed04_actividad/Main.java
@@ -11,11 +11,12 @@ public class Main {
     public static void main(String[] args) {
         CCuenta cuenta1;
         double saldoActual;
-
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
+        operativa_cuenta(cuenta1,saldoActual);
+    }
+    private static void operativa_cuenta(CCuenta cuenta1, double saldoActual){
         System.out.println("El saldo actual es"+ saldoActual );
-
         try {
             cuenta1.retirar(2300);
         } catch (Exception e) {
